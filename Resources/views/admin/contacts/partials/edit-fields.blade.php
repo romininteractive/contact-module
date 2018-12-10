af9c5932fb3c4d8ba5e82b20332977c638136d6b
mahesh baliya <mbaliya26@gmail.com>
2018-12-10 18:02:56 +0530

    update changes for tabiing issue

--- Resources/views/admin/contacts/partials/edit-fields.blade.php
+++ Resources/views/admin/contacts/partials/edit-fields.blade.php
@@ -15,20 +15,21 @@
             <div class="clearfix"></div>
       <div class="col-sm-4">
             {!! Form::label('Salutation', 'Salutation') !!}
-          {!! Form::select('salutation', $salutations, $contact->salutation, ['class' => 'form-control'] ) !!}                        <br>
+          {!! Form::select('salutation', $user_salution, $contact->salutation, ['class' => 'form-control', 'tabindex'=> '1'] ) !!}                        <br>
          <!-- {!! Form::normalSelect('salutation','Salutation',$errors,['','Mr.','Mrs.','Ms.','MISS','Dr.']) !!} -->
-         {!! Form::normalInput('email', 'Email Address', $errors,$contact) !!}
-         {!! Form::normalInput('company_name', 'Company Name', $errors,$contact) !!}
+         {!! Form::normalInput('email', 'Email Address', $errors,$contact, ['tabindex'=> '5']) !!}
+         {!! Form::normalInput('company_name', 'Company Name', $errors,$contact, ['tabindex'=> '7']) !!}
       </div>
       <div class="col-sm-4">
-        {!! Form::normalInput('first_name', 'First Name', $errors,$contact) !!}
-        {!! Form::normalInput('phone', 'Phone Number', $errors,$contact, ['class' => 'form-control','maxlength' => 10, 'onkeypress' => 'return isNumber(event)']) !!}
-        {!! Form::normalInput('department', 'Department', $errors,$contact) !!}
+        {!! Form::normalInput('first_name', 'First Name', $errors,$contact, ['tabindex'=> '2']) !!}
+       {!! Form::normalInput('last_name', 'Last Name', $errors,$contact, ['tabindex'=> '3']) !!}        
+        {!! Form::normalInput('phone', 'Phone Number', $errors,$contact, ['class' => 'form-control','maxlength' => 10, 'onkeypress' => 'return isNumber(event)', 'tabindex'=> '8']) !!}
+        {!! Form::normalInput('department', 'Department', $errors,$contact, ['tabindex'=> '9']) !!}
       </div>
       <div class="col-sm-4">
-       {!! Form::normalInput('last_name', 'Last Name', $errors,$contact) !!}
-       {!! Form::normalInput('gstin', 'GSTIN', $errors,$contact) !!}
-       {!! Form::normalInput('designation', 'Designation', $errors,$contact) !!}
+
+       {!! Form::normalInput('gstin', 'GSTIN', $errors,$contact, ['tabindex'=> '4']) !!}
+       {!! Form::normalInput('designation', 'Designation', $errors,$contact, ['tabindex'=> '6']) !!}
       </div>      
 </fieldset>
 </div>
@@ -40,14 +41,14 @@
   	  <fieldset>
     <legend>Billing Detail:</legend>
 
-  	{!! Form::normalInput('name', 'Name', $errors,$billingConatctAddress) !!}
-    	 {!! Form::normalInput('address', 'Address', $errors,$billingConatctAddress) !!}
-    	 {!! Form::normalInput('city', 'City', $errors,$billingConatctAddress) !!}
-    	 {!! Form::normalInput('state', 'State', $errors,$billingConatctAddress) !!}
-    	 {!! Form::normalInput('zip_code', 'Zip Code', $errors,$billingConatctAddress, ['class' => 'form-control','maxlength' => 6, 'onkeypress' => 'return isNumber(event)']) !!}
-    	 {!! Form::normalInput('country', 'Country', $errors,$billingConatctAddress) !!}
-    	        {!! Form::normalInput('fax', 'Fax', $errors,$billingConatctAddress) !!}
-    	         {!! Form::normalInput('billingphone', 'Phone', $errors,$billingConatctAddress, ['class' => 'form-control','maxlength' => 10, 'onkeypress' => 'return isNumber(event)']) !!}
+  	{!! Form::normalInput('name', 'Name', $errors,$billingConatctAddress, ['tabindex'=> '10']) !!}
+    	 {!! Form::normalInput('address', 'Address', $errors,$billingConatctAddress, ['tabindex'=> '12']) !!}
+    	 {!! Form::normalInput('city', 'City', $errors,$billingConatctAddress, ['tabindex'=> '14']) !!}
+    	 {!! Form::normalInput('state', 'State', $errors,$billingConatctAddress, ['tabindex'=> '16']) !!}
+    	 {!! Form::normalInput('zip_code', 'Zip Code', $errors,$billingConatctAddress, ['class' => 'form-control','maxlength' => 6, 'onkeypress' => 'return isNumber(event)','tabindex'=> '18']) !!}
+    	 {!! Form::normalInput('country', 'Country', $errors,$billingConatctAddress, ['tabindex'=> '20']) !!}
+    	        {!! Form::normalInput('fax', 'Fax', $errors,$billingConatctAddress, ['tabindex'=> '22']) !!}
+    	         {!! Form::normalInput('billingphone', 'Phone', $errors,$billingConatctAddress, ['class' => 'form-control','maxlength' => 10, 'onkeypress' => 'return isNumber(event)','tabindex'=> '24']) !!}
     	     </fieldset>
     	     </div>
   <div class="col-sm-6">
@@ -55,39 +56,39 @@
     <legend>Shipping Detail:</legend>
     <div class="form-group">
           {!! Form::label('Name', 'Name') !!}
-          {!! Form::text('sname', $shippingConatctAddress->name, ['class' => 'form-control', 'required']) !!}
+          {!! Form::text('sname', $shippingConatctAddress->name, ['class' => 'form-control', 'required','tabindex'=> '11']) !!}
   	<!-- {!! Form::normalInput('name', 'Name', $errors,$shippingConatctAddress) !!} -->
     </div>
     <div class="form-group">
           {!! Form::label('Address', 'Address') !!}
-    	    {!! Form::text('saddress', $shippingConatctAddress->address, ['class' => 'form-control', 'required']) !!}
+    	    {!! Form::text('saddress', $shippingConatctAddress->address, ['class' => 'form-control', 'required','tabindex'=> '13']) !!}
     </div>
     <div class="form-group">
           {!! Form::label('City', 'City') !!}
-          {!! Form::text('scity', $shippingConatctAddress->city, ['class' => 'form-control']) !!}
+          {!! Form::text('scity', $shippingConatctAddress->city, ['class' => 'form-control','tabindex'=> '15']) !!}
     <!-- {!! Form::normalInput('name', 'Name', $errors,$shippingConatctAddress) !!} -->
     </div>         
     <div class="form-group">
           {!! Form::label('State', 'State') !!}
-          {!! Form::text('sstate', $shippingConatctAddress->state, ['class' => 'form-control']) !!}
+          {!! Form::text('sstate', $shippingConatctAddress->state, ['class' => 'form-control','tabindex'=> '17']) !!}
     	     <!-- {!! Form::normalInput('scity', 'City1', $errors,$shippingConatctAddress) !!} -->
     </div>             
     <div class="form-group">
           {!! Form::label('ZIP Code', 'ZIP Code') !!}
-          {!! Form::text('szip_code', $shippingConatctAddress->zip_code, ['class' => 'form-control','maxlength' => 6, 'onkeypress' => 'return isNumber(event)']) !!}
+          {!! Form::text('szip_code', $shippingConatctAddress->zip_code, ['class' => 'form-control','maxlength' => 6, 'onkeypress' => 'return isNumber(event)','tabindex'=> '19']) !!}
     	     <!-- {!! Form::normalInput('sstate', 'State', $errors,$shippingConatctAddress) !!} -->
     </div>
     <div class="form-group">
           {!! Form::label('Country', 'Country') !!}
-          {!! Form::text('scountry', $shippingConatctAddress->country, ['class' => 'form-control']) !!}
+          {!! Form::text('scountry', $shippingConatctAddress->country, ['class' => 'form-control','tabindex'=> '21']) !!}
     </div>
     <div class="form-group">
           {!! Form::label('Fax', 'Fax') !!}
-          {!! Form::text('sfax', $shippingConatctAddress->fax, ['class' => 'form-control']) !!}
+          {!! Form::text('sfax', $shippingConatctAddress->fax, ['class' => 'form-control','tabindex'=> '23']) !!}
     </div>                         
     <div class="form-group">
           {!! Form::label('Phone', 'Phone') !!}
-          {!! Form::text('sbillingphone', $shippingConatctAddress->billingphone, ['class' => 'form-control','maxlength' => 10, 'onkeypress' => 'return isNumber(event)']) !!}
+          {!! Form::text('sbillingphone', $shippingConatctAddress->billingphone, ['class' => 'form-control','maxlength' => 10, 'onkeypress' => 'return isNumber(event)','tabindex'=> '25']) !!}
     </div>                             
     	    	</fieldset>
     	    	</div>
