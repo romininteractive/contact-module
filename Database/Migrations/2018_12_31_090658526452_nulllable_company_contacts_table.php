feb53fb0dbd1ff22ef67b9be9728a97a908a30c5
mbaliya26 <mbaliya26@gitlab.com>
2019-01-01 12:07:20 +0530

    update company name nullable in contact

---
+++ Database/Migrations/2018_12_31_090658526452_nulllable_company_contacts_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class NulllableCompanyContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('contact__contacts', function (Blueprint $table) {
+            $table->string('company_name')->nullable()->change();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+}
