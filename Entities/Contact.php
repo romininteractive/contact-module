c0ecca4f91f01fadab90eca5ea643eccf9207706
mbaliya26 <mbaliya26@gitlab.com>
2019-04-01 17:28:35 +0530

    working

--- Entities/Contact.php
+++ Entities/Contact.php
@@ -139,14 +139,20 @@ class Contact extends Model implements ContactInterface
     }
 
     public function gstState($gstno){
+
         $state_name = null;
-        $state_code = substr($this->gstin, 0,2);
+        $state_code = substr($gstno, 0,2);
         $states = config('asgard.contact.gst_states');
         $gst_states = $states['gst_state_code'];
         if(array_key_exists($state_code, $gst_states)){
             $state_name = $gst_states[$state_code];
         }        
         return $state_name;         
+    }
+
+    public function getGstStateCode(){        
+        $state_code = substr($this->gstin, 0,2);        
+        return $state_code;
     }     
 }
 
